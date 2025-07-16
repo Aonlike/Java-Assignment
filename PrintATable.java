592eec5a3a3c4ac0ae537a58e3edfa70e1c3b034
Martin Ngutswen <martinngutswen02@gmail.com>
2025-07-16 09:15:22 +0100

    first commit

---
+++ PrintATable.java
@@ -0,0 +1,10 @@
+public class PrintATable{
+		public static void main(String[] args){
+
+	for(int a = 1; a <= 5; a++) {
+		for(int b = 2; b <= 6; b++) System.out.print((int)Math.pow(a, b) + "\t");
+		System.out.println();
+		}
+
+ }
+}
